e733e817b6eed4733374988950d9f550862a2d3e
Alessandro <alembfilho@gmail.com>
2022-04-29 00:18:16 -0300

    formatting

--- trivia/src/components/Home.jsx
+++ trivia/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import '../styles/Home.scss'
 
-export default function Home ({startGame}){
+export default function Home({ startGame }) {
   return <div className='home'>
     <h1>Quizzical</h1>
     <p>It's time to test your knowledge!</p>
